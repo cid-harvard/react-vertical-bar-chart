2652a560d8261d4217afa31e635df184418f0c2a
Kyle Soeltz <wsoeltz@gmail.com>
2021-02-05 14:30:20 -0500

    Updates

---
+++ precache-manifest.a7fe7dbbefaf05ad4502cd3e1c39af6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19fb72d541e23cb1261f2b75c91a3df5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "19179c9105e9b7943dc8",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "75c082dec80123102f39",
+    "url": "./static/js/2.6e52275a.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "./static/js/2.6e52275a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19179c9105e9b7943dc8",
+    "url": "./static/js/main.7758a0d2.chunk.js"
+  },
+  {
+    "revision": "122c629799faee2a1e8b",
+    "url": "./static/js/runtime-main.11405e85.js"
+  }
+]);
